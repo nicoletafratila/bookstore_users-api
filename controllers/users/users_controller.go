2e5b4f2cf47d4da2622725f5272521fa2269e699
nicoleta.fratila <nicoleta.fratila@softvision.com>
2021-07-15 10:44:03 +0300

    make module

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -37,7 +37,7 @@ func Create(c *gin.Context) {
 
 func Get(c *gin.Context) {
 	if err := oauth.AuthenticateRequest(c.Request); err != nil {
-		c.JSON(err.Status, err)
+		c.JSON(err.Status(), err)
 		return
 	}
 
